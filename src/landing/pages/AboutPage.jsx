ad2b39997b88c40fd33a49c90ba9c58ceef69eca
EmmaG2 <itsmeemmadev@gmail.com>
2022-11-23 20:54:43 -0300

    [landing] about text was added

--- src/landing/pages/AboutPage.jsx
+++ src/landing/pages/AboutPage.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
 
 export const AboutPage = () => {
-  return <div>AboutPage</div>
+  return (
+    <section>
+      <h1>Sobre nosotros</h1>
+      <p>
+        SegDattax es un sistema creado para que el usuario pueda conocer, toda
+        su información personal a la que la empresa tiene acceso.
+      </p>
+      <p>
+        Este sistema puede ser implementado por cualquier empresa para que sus
+        usuarios puedan saber la informacion que se tiene tiene almacenadad de
+        ellos y para que es usada
+      </p>
+    </section>
+  )
 }
