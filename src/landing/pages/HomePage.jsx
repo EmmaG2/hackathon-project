37d957d8bcbaed8ac648343e47ec7ec9f00b9652
EmmaG2 <itsmeemmadev@gmail.com>
2022-11-23 20:16:14 -0300

    [landing] text updated

--- src/landing/pages/HomePage.jsx
+++ src/landing/pages/HomePage.jsx
@@ -1,4 +1,16 @@
+import {Typography} from '@mui/material'
 import React from 'react'
 export const HomePage = () => {
-  return <></>
+  return (
+    <>
+      <Typography
+        component={'h1'}
+        variant={'h3'}
+        sx={{
+          color: 'primary.main',
+        }}>
+        Home Page
+      </Typography>
+    </>
+  )
 }
