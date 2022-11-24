40f25963330887acf1c0fc294dd771141bfa1ae6
EmmaG2 <itsmeemmadev@gmail.com>
2022-11-24 06:55:34 -0300

    [admin] pages updated

--- src/admin/pages/HomePage.jsx
+++ src/admin/pages/HomePage.jsx
@@ -8,9 +8,19 @@ export const HomePage = () => {
     <Grid container>
       <Grid item>
         <Typography>
-          Bienvenido <Typography>{displayName}</Typography>
+          Bienvenido
+          <Typography
+            sx={{
+              color: 'secondary.main',
+              // fontWeight: 700,
+              ml: 0.5,
+            }}
+            component={'span'}>
+            {displayName}
+          </Typography>
         </Typography>
       </Grid>
+      <Grid item>Esta es la información que manejas de tus usuarios</Grid>
     </Grid>
   )
 }
