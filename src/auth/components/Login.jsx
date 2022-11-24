08f965b4a829a5c606c436740a57198398077ac0
EmmaG2 <itsmeemmadev@gmail.com>
2022-11-24 13:46:54 -0300

    [auth] logo was added

--- src/auth/components/Login.jsx
+++ src/auth/components/Login.jsx
@@ -2,6 +2,8 @@ import {useDispatch} from 'react-redux'
 import {Grid, Typography, Box, Button} from '@mui/material'
 import React from 'react'
 import {startGoogleSignIn} from '../../context/slices/auth'
+import {LazyLoadImage} from 'react-lazy-load-image-component'
+import 'react-lazy-load-image-component/src/effects/blur.css'
 
 export const Login = () => {
   const dispatch = useDispatch()
@@ -19,15 +21,17 @@ export const Login = () => {
       sx={{
         heigth: '100%',
       }}>
-      <Typography component='h1' fontSize={25} mb={5}>
-        Por favor, haz sesion con Google
+      <Typography component='h1' fontSize={'2rem'} mb={5}>
+        Por favor, inica sesión con tu cuenta de Google
       </Typography>
 
       <Box w={100}>
         <Button onClick={onGoogleSignIn} variant='outlined' color='primary'>
-          <img
+          <LazyLoadImage
+            effect='blur'
             src='https://www.freepngimg.com/thumb/google/67058-logo-search-google-icon-free-clipart-hq.png'
-            width={60}></img>
+            width={60}
+          />
         </Button>
       </Box>
     </Grid>
