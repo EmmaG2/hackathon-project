19fce544d1ab7fe7c7ae445270a67558150ea9f8
EmmaG2 <itsmeemmadev@gmail.com>
2022-11-24 06:56:02 -0300

    [firebase] more providers were added

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,29 +1,32 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
-import { firebaseAuth } from './firebaseConfig'
+import {GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import {firebaseAuth} from './firebaseConfig'
 
 const googleProvider = new GoogleAuthProvider()
 
 export const signInWithGoogle = async () => {
-    try {
-      const result = await signInWithPopup(firebaseAuth, googleProvider)
-      const { displayName, email, photoURL, uid } = result.user
-  
-      return {
-        ok: true,
-        displayName,
-        email,
-        photoURL,
-        uid,
-      }
-    } catch (error) {
-      const errorCode = error.code
-      const errorMessage = error.message
-  
-      return {
-        ok: false,
-        errorCode,
-        errorMessage,
-      }
+  try {
+    const result = await signInWithPopup(firebaseAuth, googleProvider)
+    const {displayName, email, photoURL, uid} = result.user
+
+    return {
+      ok: true,
+      displayName,
+      email,
+      photoURL,
+      uid,
+    }
+  } catch (error) {
+    const errorCode = error.code
+    const errorMessage = error.message
+
+    return {
+      ok: false,
+      errorCode,
+      errorMessage,
     }
   }
-  
+}
+
+export const onLogoutFirebase = async () => {
+  return await firebaseAuth.signOut()
+}
