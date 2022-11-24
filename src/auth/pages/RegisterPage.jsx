e432a1fc17084149fc3f43c2ea30bf4794738170
EmmaG2 <itsmeemmadev@gmail.com>
2022-11-24 02:54:19 -0300

    [auth] register page was created

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
-import Login from '../components/Login'
+import {Login, UserCard} from '../components'
+import {useSelector} from 'react-redux'
+import {Typography} from '@mui/material'
+import {Box} from '@mui/system'
 
 export const RegisterPage = () => {
-  return <>
+  const {status} = useSelector((state) => state.auth)
 
-    <Login/>
-  </>
+  return (
+    <Box
+      flex
+      justifyContent={'center'}
+      align='center'
+      sx={{
+        width: '100%',
+      }}>
+      <Login />
+
+      {status === 'authenticated' ? (
+        <Box>
+          <Typography
+            sx={{
+              mt: 2,
+              mb: 2,
+            }}>
+            Autorizado con exito
+          </Typography>
+          <UserCard />
+        </Box>
+      ) : null}
+    </Box>
+  )
 }
