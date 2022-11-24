7af8da84eaa8e5f32d2914ec97bb272ba6b004d9
EmmaG2 <itsmeemmadev@gmail.com>
2022-11-24 04:39:45 -0300

    [admin] admin pages were created

---
+++ src/admin/pages/AddData.jsx
@@ -0,0 +1,12 @@
+import {Grid, Typography} from '@mui/material'
+import React from 'react'
+
+export const AddData = () => {
+  return (
+    <Grid container>
+      <Grid item>
+        <Typography>Working</Typography>
+      </Grid>
+    </Grid>
+  )
+}
