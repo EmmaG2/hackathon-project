776efab00f6f72fcb54d229c7a780decb65c390f
EmmaG2 <itsmeemmadev@gmail.com>
2022-11-24 06:55:13 -0300

    [auth] routes updated

--- src/auth/routes/AuthRouter.jsx
+++ src/auth/routes/AuthRouter.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import {Route, Routes} from 'react-router-dom'
+import {Navigate, Route, Routes} from 'react-router-dom'
+import {useCheckAuth} from '../../global/hooks'
 import {RegisterPage} from '../pages'
 
 export const AuthRouter = () => {
+  const {status} = useCheckAuth()
+
   return (
     <Routes>
-      <Route path='/register' element={<RegisterPage />} />
+      {status === 'authenticated' ? (
+        <Route path='/*' element={<Navigate to={'/landing/home'} />} />
+      ) : (
+        <Route path='/register' element={<RegisterPage />} />
+      )}
     </Routes>
   )
 }
