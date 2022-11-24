0a4c0809d4cdb5773f6c8c4ee72c6a67e70788dd
Fer8-8 <feranandonajerarascon@Gmail.com>
2022-11-24 11:45:09 -0600

    Updated section about

--- src/landing/pages/AboutPage.jsx
+++ src/landing/pages/AboutPage.jsx
@@ -5,13 +5,24 @@ export const AboutPage = () => {
     <section>
       <h1>Sobre nosotros</h1>
       <p>
-        SegDattax es un sistema creado para que el usuario pueda conocer, toda
-        su información personal a la que la empresa tiene acceso.
+        SepegDattax es un proyecto que soluciona un problema de gran relevancia
+        hoy en dia, el cual es el uso de los datos personales de los usuarios.
       </p>
+      <br />
+      <h2>Para los usuarios</h2>
       <p>
-        Este sistema puede ser implementado por cualquier empresa para que sus
-        usuarios puedan saber la informacion que se tiene tiene almacenadad de
-        ellos y para que es usada
+        Con SegDattax los usuarios podran consultar la informacion que tiene las
+        empresas sobre ellos, y si se deseea se podra eliminar los datos que no
+        quieran ser compartidos con la empresa.
+      </p>
+
+      <br />
+      <h2>Para las empresas</h2>
+      <p>
+        Con SegDattax las empresas podran tener un sistema facil de usar donde
+        podran gestionar la informacion de los usuarios los cuales tendran la
+        seguridad de que sus datos seran utilizados de manera correcta generando
+        una buena imagen y confianza en la empresa.
       </p>
     </section>
   )
